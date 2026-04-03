94724300735e141afe064db9da56a0aab5c5d875
gunderbus <cole.gundermann@gmail.com>
2026-04-03 05:13:19 -0400

    horiyima

--- drawrect.py
+++ drawrect.py
@@ -7,3 +7,15 @@ class drawRectObj:
     width: float
     height: float
     color: pygame.Color
+
+    def init(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def drawThisRect(self, xdil: float, ydil: float, screen):
+        pygame.draw.rect(
+            screen, self.color, (self.x + xdil, self.y + ydil, self.width, self.height)
+        )
