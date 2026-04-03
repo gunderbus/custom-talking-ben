02c1500c12e788301487e00301cb395d57bdf0a6
gunderbus <cole.gundermann@gmail.com>
2026-04-03 05:55:51 -0400

    testing ts

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 # import dspy
+import this
+
 import pygame
+
+import drawrect
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+cole = drawrect.drawRectObj(10, 10, 100, 100, pygame.Color(255, 255, 255))
+
 
 while running:
     # poll for events
@@ -20,6 +26,7 @@ while running:
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
+    cole.drawThisRect(0, 0, 10, screen)
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
