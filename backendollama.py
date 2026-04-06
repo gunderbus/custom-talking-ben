cb6067d9b3fc248526971a0bd0e22e44cbfc33e1
gunderbus <cole.gundermann@gmail.com>
2026-04-06 10:13:21 -0400

    bro ishikawa is such a fucking LOSER

--- backendollama.py
+++ backendollama.py
@@ -9,18 +9,24 @@ class AICommunicator:
         imodeltype: str,
     ):
         self.modeltype = imodeltype
-
+        self.context = "No current context"
         try:
             self.model = dspy.OllamaLocal(model=self.modeltype)
             dspy.settings.configure(lm=self.model)
         except ValueError:
             print("Please use an actual str for modeltype")
 
+    def readFile(self, filePath: str):
+        with open(filePath, "r") as file:
+            content = file.read()
+            self.context = (
+                "Here is the environment context to understand each question to ask: "
+                + content
+            )
+
     def callts(self, prompt: str):
         try:
-            # 2. Define the module (Strategy)
             qa_module = dspy.ChainOfThought("question -> answer")
-
             return qa_module(question=prompt)
         except ValueError:
             print("Please use an ACTUAL prompt")
